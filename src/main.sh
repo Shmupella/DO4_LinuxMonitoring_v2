da04360abb45c7bbff54aa97ac7bdab16e367fdf
shmupella <emilynovael@gmail.com>
2023-03-06 19:23:40 +0300

    ready 1 task

--- src/main.sh
+++ src/main.sh
@@ -3,8 +3,6 @@
 . ./check.sh
 . ./create.sh
 
-chmod +x check.sh create.sh
-
 path=$1
 count_d=$2
 dir_letters=$3
@@ -16,3 +14,6 @@ count_param=$#
 
 check $path $count_d $dir_letters $count_f $file_letters $file_size $count_param
 creation_directory_with_files
+
+find $1 -type d -cmin -20 -ls | awk '{printf "%s %s%s %s\n", $11, $9, $8, $10}' > info.log #create log for directory
+find $1 -type f -cmin -20 -ls | awk '{printf "%s %s%s %s %sKb\n", $11, $9, $8, $10, $7}' >> info.log #create log for file
