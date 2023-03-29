63a15bf13e8ffa49e2af237ff4ec6556af0138c1
shmupella <emilynovael@gmail.com>
2023-03-29 22:52:21 +0300

    3 task is ready, need to add check on input

--- src/02/remove.sh
+++ src/02/remove.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 count_rm_path=$(awk 'END{print NR}' infod.log)
-
 i=1
-while [[ $i -lt $count_rm_path ]]
+
+while [[ $i -le $count_rm_path ]]
 do
     path_name=$(awk 'NR == '$i' {print $1}' infod.log)
-    sudo rm -rf "$path_name"
+    sudo rm -vr "$path_name"
     ((i++))
 done
+
+rm "path_dir"
